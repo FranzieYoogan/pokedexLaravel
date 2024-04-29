e2d3094707a38d1fcacabb403d47d041f46e6773
FrankieSilvaSantos <frankiesilvasantos@gmail.com>
2024-04-28 23:56:41 -0300

    pokedexBeginning

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    rel="stylesheet"
+  />
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    rel="stylesheet"
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+
+<link rel="stylesheet" href="{{asset('/css/footer.css')}}">
+
+</head>
+<body>
+
+    <!-- Footer -->
+<footer class="" style="position: absolute; bottom: 0; width: 100%">
+    <!-- Grid container -->
+   
+  
+    <!-- Copyright -->
+    <div class="textStyle1 text-center p-3" style="background-color: white; border-top: 3px solid black">
+      
+
+          <!-- Facebook -->
+
+          <div>
+
+     
+          <a data-mdb-ripple-init class="btnIcon btn btn-outline btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-facebook-f"></i
+        ></a>
+  
+        <!-- Twitter -->
+        <a data-mdb-ripple-init class="btnIcon btn btn-outline btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-twitter"></i
+        ></a>
+  
+        <!-- Google -->
+        <a data-mdb-ripple-init class="btnIcon btn btn-outline btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-google"></i
+        ></a>
+  
+        <!-- Instagram -->
+        <a data-mdb-ripple-init class="btnIcon btn btn-outline btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-instagram"></i
+        ></a>
+  
+        <!-- Linkedin -->
+        <a data-mdb-ripple-init class="btnIcon btn btn-outline btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-linkedin-in"></i
+        ></a>
+  
+        <!-- Github -->
+        <a data-mdb-ripple-init class="btnIcon btn btn-outline btn-floating m-1" href="#!" role="button"
+          ><i class="fab fa-github"></i
+        ></a>
+
+        <div>
+            © 2020 Copyright:
+            <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        </div>
+    </div>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+    
+</body>
+</html>
